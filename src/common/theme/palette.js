f1ae20b3aa3bbd01902438109b19fb7552b592fa
emmanuelh-dev <86961961+emmanuelh-dev@users.noreply.github.com>
2023-11-28 15:53:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/common/theme/palette.js
@@ -0,0 +1,22 @@
+import { grey, green, indigo } from '@mui/material/colors';
+
+const validatedColor = (color) => (/^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null);
+
+export default (server, darkMode) => ({
+  mode: darkMode ? 'dark' : 'light',
+  background: {
+    default: darkMode ? grey[900] : grey[50],
+  },
+  primary: {
+    main: '#0dd3ba',
+  },
+  secondary: {
+    main: '#0dd3ba',
+  },
+  neutral: {
+    main: grey[500],
+  },
+  geometry: {
+    main: '#3bb2d0',
+  },
+});
