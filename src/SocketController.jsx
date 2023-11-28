fe19b827392d818fa68e76f3cf46bd56b00db64d
Emmanuel Hernandez <e805177@gmail.com>
2023-11-28 11:50:05 -0600

    Feat: host was changed

--- src/SocketController.jsx
+++ src/SocketController.jsx
@@ -33,7 +33,7 @@ const SocketController = () => {
 
   const connectSocket = () => {
     const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-    const socket = new WebSocket(`${protocol}//${window.location.host}/api/socket`);
+    const socket = new WebSocket(`${protocol}//${import.meta.env.VITE_SOCKET_URL}/api/socket`);
     socketRef.current = socket;
 
     socket.onopen = () => {
--- src/login/ChangeServerPage.jsx
+++ src/login/ChangeServerPage.jsx
@@ -7,7 +7,7 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from '../common/components/LocalizationProvider';
 
-const currentServer = `${window.location.protocol}//${window.location.host}`;
+const currentServer = `${window.location.protocol}//${import.meta.env.VITE_API_URL}`;
 
 const officialServers = [
   currentServer,
