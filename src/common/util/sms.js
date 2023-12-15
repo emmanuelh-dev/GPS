b9b25121c0a28b573b101546df63d0cae7789d8a
Emmanuel Hernandez <e805177@gmail.com>
2023-12-15 16:27:02 -0600

    Update sms.js

--- src/common/util/sms.js
+++ src/common/util/sms.js
@@ -7,44 +7,60 @@ myHeaders.append(
   'Basic MjFmMjg0OTk4NmJlMTVjZjJhN2Q2ZmMzM2YxNjZjOGFkY2JhNjFiYTlmMDhlYWQ0NTg2YzlhM2ExNWE1MGE5MjpFQi1tdXBYUTBWWkFadVZsQkYzYlZuMzRTaTh1YTIzbzFhLUJvN1FKODVIS2FoYVVaSXBBVHVSYVhZMnhDdlgyOWRfNlBaVnBQbkJSdmw1X3d4WEVNUQ==',
 );
 
-const raw = JSON.stringify({
-  text: 'fix090s***n123456',
-});
+function sms({ phoneNumber, message }) {
+  const raw = JSON.stringify({
+    icc: phoneNumber,
+    message,
+  });
 
-const requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow',
-};
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
 
-function sms({ phoneNumber }) {
   if (phoneNumber.length === 19) {
-    fetch(
-      `http://119.8.11.135/api/icc/${phoneNumber}/send_sms`,
-      requestOptions,
-    )
-      .then((response) => response.text())
-      .then((result) => toast.success(`Mensaje enviado a ${result}`))
-      .catch((error) => toast.error(`Error: ${error}`));
-    return;
+    if (phoneNumber.length === 19) {
+      setTimeout(() => {
+        fetch(
+          'https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-5075ff73-6671-403d-9b7e-7e0ca64f2ccb/default/sms',
+          requestOptions,
+        )
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+          })
+          .then((result) => {
+            toast.success(`Mensaje enviado correctamente: ${result}`);
+          })
+          .catch((error) => {
+            toast.error(`Error: ${error.message}`);
+          });
+      }, 2000); // Retraso de 2 segundos
+      return;
+    }
+
+    toast.error('Este dispositivo no tiene número de teléfono válido');
   }
   toast.error('Este dispositivo no tiene numero de telefono valido');
 }
 
-export function stopMotor({ phoneNumber, deviceName }) {
-  sms({ phoneNumber, message: 'quickstop123456' });
+export async function stopMotor({ phoneNumber }) {
+  await sms({ phoneNumber, message: 'quickstop123456' });
 }
 
-export function runMotor({ phoneNumber, deviceName }) {
-  sms({ phoneNumber, message: 'quickstop123456' });
+export async function runMotor({ phoneNumber }) {
+  await sms({ phoneNumber, message: 'quickstop123456' });
 }
 
-export function configDevice({ phoneNumber, deviceName }) {
+export async function configDevice({ phoneNumber }) {
   // DNS configuration
-  sms({ phoneNumber, message: 'apn123456 m2mglobal.telefonica.mx' });
+  await sms({ phoneNumber, message: 'apn123456 m2mglobal.telefonica.mx' });
   // Timeout configuration
-  sms({ phoneNumber, message: 'fix090s***n123456' });
+  await sms({ phoneNumber, message: 'fix090s***n123456' });
   // APN Configuration
-  sms({ phoneNumber, message: 'dns123456 24.199.121.252 5001' });
+  await sms({ phoneNumber, message: 'dns123456 24.199.121.252 5001' });
 }
