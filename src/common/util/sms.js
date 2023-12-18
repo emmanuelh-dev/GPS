d3c9c97fc07a615a88017c783fdd46b177bbcec3
Emmanuel Hernandez <e805177@gmail.com>
2023-12-18 09:44:23 -0600

    Update sms.js

--- src/common/util/sms.js
+++ src/common/util/sms.js
@@ -7,10 +7,11 @@ myHeaders.append(
   'Basic MjFmMjg0OTk4NmJlMTVjZjJhN2Q2ZmMzM2YxNjZjOGFkY2JhNjFiYTlmMDhlYWQ0NTg2YzlhM2ExNWE1MGE5MjpFQi1tdXBYUTBWWkFadVZsQkYzYlZuMzRTaTh1YTIzbzFhLUJvN1FKODVIS2FoYVVaSXBBVHVSYVhZMnhDdlgyOWRfNlBaVnBQbkJSdmw1X3d4WEVNUQ==',
 );
 
-function sms({ phoneNumber, message }) {
+function sms({ phoneNumber, message, messages }) {
   const raw = JSON.stringify({
     icc: phoneNumber,
     message,
+    messages,
   });
 
   const requestOptions = {
@@ -22,24 +23,23 @@ function sms({ phoneNumber, message }) {
 
   if (phoneNumber.length === 19) {
     if (phoneNumber.length === 19) {
-      setTimeout(() => {
-        fetch(
-          'https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-5075ff73-6671-403d-9b7e-7e0ca64f2ccb/default/sms',
-          requestOptions,
-        )
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.text();
-          })
-          .then((result) => {
-            toast.success(`Mensaje enviado correctamente: ${result}`);
-          })
-          .catch((error) => {
-            toast.error(`Error: ${error.message}`);
-          });
-      }, 2000); // Retraso de 2 segundos
+      fetch(
+        'https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-5075ff73-6671-403d-9b7e-7e0ca64f2ccb/default/sms',
+        requestOptions,
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.text();
+        })
+        .then((result) => {
+          toast.success(`Mensaje enviado correctamente: ${result}`);
+        })
+        .catch((error) => {
+          toast.error(`Error: ${error.message}`);
+        });
+
       return;
     }
 
@@ -48,19 +48,17 @@ function sms({ phoneNumber, message }) {
   toast.error('Este dispositivo no tiene numero de telefono valido');
 }
 
-export async function stopMotor({ phoneNumber }) {
-  await sms({ phoneNumber, message: 'quickstop123456' });
+// Stops motor function
+export function stopMotor({ phoneNumber }) {
+  sms({ phoneNumber, message: 'quickstop123456' });
 }
 
-export async function runMotor({ phoneNumber }) {
-  await sms({ phoneNumber, message: 'quickstop123456' });
+// Starts motor function
+export function runMotor({ phoneNumber }) {
+  sms({ phoneNumber, message: 'quickstop123456' });
 }
 
-export async function configDevice({ phoneNumber }) {
-  // DNS configuration
-  await sms({ phoneNumber, message: 'apn123456 m2mglobal.telefonica.mx' });
-  // Timeout configuration
-  await sms({ phoneNumber, message: 'fix090s***n123456' });
-  // APN Configuration
-  await sms({ phoneNumber, message: 'dns123456 24.199.121.252 5001' });
+// Configures devices function
+export function configDevice({ phoneNumber }) {
+  sms({ phoneNumber, messages: ['apn123456 m2mglobal.telefonica.mx', 'angle123456 60', 'fix090s***n123456', 'dns123456 24.199.121.252 5001'] });
 }
